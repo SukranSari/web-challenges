4942ee57b5712b55c75c017919b8d8da2fdce380
SukranSari <sukran_sari@hotmail.de>
2024-06-05 17:00:23 +0200

    Lösungen aktualisiert

--- js-conditions-and-booleans_statements/index.js
+++ js-conditions-and-booleans_statements/index.js
@@ -1,15 +1,57 @@
 console.clear();
 
 // Part 1: Password
-const SUPER_SECRET_PASSWORD = "h4x0r1337";
+const SUPER_SECRET_PASSWORD = "h4x0r13371";
+
+if (SUPER_SECRET_PASSWORD == "h4x0r1337") {
+  // code that is executed only if condition "isSunShining" is true
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  // code that is executed only if condition "isSunShining" is false
+  console.log("Access denied!");
+}
+
+/*************************************************************************************************************/
 
 const receivedPassword = "password1234";
 
+const check = receivedPassword
+  ? console.log("Welcome! You are logged in as Brunhilde1984.")
+  : console.log("Access denied!");
+
+/*************************************************************************************************************/
+
 // Part 2: Even / Odd
-const number = 6;
+const number = 5;
+
+if (number % 2) {
+  console.log("Odd");
+} else {
+  console.log("Even");
+}
+
+/*************************************************************************************************************/
+
+const numberr = 8;
+
+if (numberr == 8) {
+  console.log("EVEN");
+}
+
+/*************************************************************************************************************/
 
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 2;
+
+if (numberOfHotdogs < 5) {
+  console.log("2 euro per hotdog");
+} else if (numberOfHotdogs < 18) {
+  console.log("Good afternoon.");
+} else if (numberOfHotdogs === 24) {
+  console.log("Good night.");
+} else {
+  console.log("Good evening.");
+}
 
 // Part 4: Daytime
 const currentHour = 12;
